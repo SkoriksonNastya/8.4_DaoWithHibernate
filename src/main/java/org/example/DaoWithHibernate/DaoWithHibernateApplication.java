235ffd3fed0f942b2740cbbe18a35eb48e4b0e47
Anastasia <nastya.skorohodova.96@mail.ru>
2023-12-24 12:27:25 +0300

    Initial commit

---
+++ src/main/java/org/example/DaoWithHibernate/DaoWithHibernateApplication.java
@@ -0,0 +1,14 @@
+package org.example.DaoWithHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoWithHibernateApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DaoWithHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/DaoWithHibernate/DaoWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.DaoWithHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
